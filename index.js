7ff469df1309e4ebeec47b378e1bb4eb13b365ee
Luis Miguel Valladolid <81938113+luisvalladolid@users.noreply.github.com>
2021-05-05 11:20:07 +0800

    Commit 3: connected to the database and started server

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
@@ -12,4 +17,15 @@ mysqlConnection.connect((err) => {
         console.log('DB connection succeeded.');
     else
         console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-})
+});
+
+app.listen(3000,()=>console.log('Express server is running at port no : 3000'));
+
+app.get('/employees', (res, req)=>{
+    mysqlConnection.query('SELECT * FROM items', (err, rows, fields) =>{
+        if(!err)
+        console.log(rows);
+        else
+        console.log(err);
+    });
+});
